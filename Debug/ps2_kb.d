8b318c2ecbc0386a9b7f3f615f0de6783a62c9db
Adrian Dudau <adrian.dudau@enea.com>
2013-05-22 13:20:53 +0200

    create and populate .gitignore

--- Debug/ps2_kb.d
+++
@@ -1,7 +0,0 @@
-ps2_kb.d ps2_kb.o: ../ps2_kb.c ../scancodes.h ../ext_keys.h ../ps2_kb.h
-
-../scancodes.h:
-
-../ext_keys.h:
-
-../ps2_kb.h:
--- Debug/usbdrv/oddebug.d
+++
@@ -1,6 +0,0 @@
-usbdrv/oddebug.d usbdrv/oddebug.o: ../usbdrv/oddebug.c \
- ../usbdrv/oddebug.h ../usbdrv/usbportability.h
-
-../usbdrv/oddebug.h:
-
-../usbdrv/usbportability.h:
